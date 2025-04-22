4c40b980dfced481173cb362734f49ac7389d481
Alex <zigzaksasha@gmail.com>
2025-04-22 21:48:56 +0200

    The Mother Nature

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
@@ -13,8 +15,9 @@ class Animal:
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name},"
-              f" Health: {self.health}, "
-              f"Hidden: {self.hidden}}}")
+                f" Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
 
 class Herbivore(Animal):
     def hide(self) -> None:
@@ -22,7 +25,7 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, other) -> None:
+    def bite(self, other: "Animal") -> None:
         if isinstance(other, Herbivore) and isinstance(self, Carnivore):
             if not other.hidden:
                 other.health = other.health - 50
