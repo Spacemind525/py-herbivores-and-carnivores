409c5e200e81c13b77494d1d5049a13614fbebe0
Alex <zigzaksasha@gmail.com>
2025-04-22 21:31:13 +0200

    The Nature

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.__class__.alive.append(self)
+
+    def check_alive(self) -> None:
+        if self.health <= 0:
+            self.__class__.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name},"
+              f" Health: {self.health}, "
+              f"Hidden: {self.hidden}}}")
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, other) -> None:
+        if isinstance(other, Herbivore) and isinstance(self, Carnivore):
+            if not other.hidden:
+                other.health = other.health - 50
+                other.check_alive()
+        print(self.__class__.alive)
